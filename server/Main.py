bfe3786886d0fef064b241fa9f8900169a12f2d4
Simon Kindhauser <simon.kindhauser@gmx.ch>
2016-02-16 18:38:09 +0100

    upgraded application key as parameter or random uuid4

--- server/Main.py
+++ server/Main.py
@@ -1,4 +1,5 @@
 import os
+from uuid import uuid4
 from flask import Flask
 from flask_restful import Api
 
@@ -8,11 +9,31 @@ from persistence.InMemoryPageRepository import InMemoryPageRepository
 from services.ApiService import ApiService
 from services.AuthenticationService import AuthenticationService
 
-if __name__ == '__main__':
+import sys, getopt
+
+def main(argv):
+	key = ''
+	try:
+		opts, args = getopt.getopt(argv,"hk:",["key="])
+	except getopt.GetoptError:
+		print ('main.py [--key <token key>]')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print ('main.py [--key <token key>]')
+			sys.exit()
+		elif opt in ("-k", "--key"):
+			key = arg
+
 	print("BLESC's SimpleCms")
 	app = Flask(__name__, static_folder='..')
 	ApiService(app, InMemoryPageRepository())
-	AuthenticationService(app)
+
+	if (key == ''):
+		key = str(uuid4().hex)
+	AuthenticationService(app, key)
 	# SqlitePageRepository("../data/content.db")
 
 	app.run("0.0.0.0", 8080, debug=True)
+if __name__ == "__main__":
+	main(sys.argv[1:])
--- server/services/AuthenticationService.py
+++ server/services/AuthenticationService.py
@@ -10,24 +10,27 @@ class User(object):
     def __str__(self):
         return "User(id='%s')" % self.id
 
-users = [
-    User(1, 'simpleAdmin', 'ThisIsAnInsecureAndTemporaryPasswordAndMustBeReplacedASAP'),
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
 
 class AuthenticationService(object):
-	def __init__(self, app):
-		app.config['SECRET_KEY'] = 'super-secret'
+	def __init__(self, app, key):
+		app.config['SECRET_KEY'] = key
 		JWT(app, self.authenticate, self.identity)
 
 	@staticmethod
 	def authenticate(username, password):
+		"""		
 		user = username_table.get(username, None)
 		if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
 			return user
+		"""
+				
+
+		if username == "simpleAdmin" and password == "tmp":
+			return User(1, "simpleAdmin", "tmp")
 	@staticmethod
 	def identity(payload):
+		"""
 		user_id = payload['identity']
 		return userid_table.get(user_id, None)
+		"""
+		return User(1, "simpleAdmin", "tmp")
