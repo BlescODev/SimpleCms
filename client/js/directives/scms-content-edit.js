4a6459af5a30349503024557f8059e901bfbeeee
Simon Kindhauser <simon.kindhauser@gmx.ch>
2016-03-17 14:11:33 +0100

    Implemented the resizing texteditor Known issues: - does not resize on init - resets view position

--- client/js/directives/scms-content-edit.js
+++ client/js/directives/scms-content-edit.js
@@ -1,4 +1,4 @@
-app.directive('scmsContentEdit', function($compile){
+app.directive('scmsContentEdit', function($compile) {
 	return {
 		restrict: 'E',
 		scope: {
@@ -6,17 +6,21 @@ app.directive('scmsContentEdit', function($compile){
 			content: "="
 		},
 		templateUrl: "/html/directives/scms-content-edit.tpl.html",
-		compile: function(tElement, tAttr) {
+		compile: function(tElement) {
 			var contents = tElement.contents().remove();
-			var compiledContents;
-
-			return function(scope, iElement, iAttr) {
-				if(!compiledContents) {
-					compiledContents = $compile(contents);
+			return {
+				pre: function(scope) {
+					scope.resize = function() {
+						var ta = document.getElementById("ta");
+						ta.style.height = 'auto';
+						ta.style.height = ta.scrollHeight + 'px';
+					}
+				},
+				post: function(scope, iElement) {
+					$compile(contents)(scope, function(clone) {
+						iElement.append(clone);
+					});
 				}
-				compiledContents(scope, function(clone, scope) {
-					iElement.append(clone); 
-				});
 			};
 		}
 	};
