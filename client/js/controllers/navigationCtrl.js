9270ef12a9fd50fa7720a9c677841f93bf47ea8a
Simon Kindhauser <simon.kindhauser@gmx.ch>
2016-03-31 19:46:55 +0200

    fixed simpleCms.svg

--- client/js/controllers/navigationCtrl.js
+++ client/js/controllers/navigationCtrl.js
@@ -1,36 +1,60 @@
-app.controller('navigationCtrl', ['appService', '$scope', 'pageService', 'user', function (appService, $scope, pageService, user) {
+app.controller('navigationCtrl', ['appService', '$scope', 'pageService', 'user', function(appService, $scope,
+	pageService, user) {
 
-    //$scope.loginText = "Login";
-    $scope.scrolled = false;
+	$scope.scrolled = false;
 
-    var init = function () {
-	navigation = appService.get('navigation').substr(1)
-        pageService.get(navigation).then(function (response) {
-            $scope.navigation = response.data.content;
-        }, function (response) {
-            appService.notifications.queue({
-                message: "Unable to retrieve navigation data!",
-                type: "error"
-            });
-        });
-    };
+	$scope.nav = [];
 
-    appService.ready(init);
+	$scope.inNav = [{
+			"title": "SimpleCms",
+			"url": "",
+			"children": [{
+				"title": "Edit",
+				"url": "#/edit/{{current.page}}"
+			},{
+				"title": "Logout",
+				"url": ""
+			}]
+		}];
 
-    window.addEventListener('scroll', function (e) {
-        $scope.scrolled = window.pageYOffset != 0;
-        $scope.$apply();
-    });
+	$scope.outNav = [{
+			"title": "SimpleCms",
+			"url": "",
+			"children": [{
+				"title": "Login",
+				"url": "#/login"
+			}]
+		}];
 
-    $scope.isAuthenticated = function () {
-        return user.isLoggedIn();
-    };
 
-    $scope.logout = function () {
-        user.logout();
-        appService.$location.url("/login")
-    };
 
+	var init = function() {
+		navigation = appService.get('navigation').substr(1)
+		pageService.get(navigation).then(function(response) {
+			$scope.nav = response.data.content; 
+		}, function(response) {
+			appService.notifications.queue({
+				message: "Unable to retrieve navigation data!",
+				type: "error"
+			});
+		});
+	};
+
+	appService.ready(init);
+
+	window.addEventListener('scroll', function(e) {
+		$scope.scrolled = window.pageYOffset != 0;
+		$scope.$apply();
+	});
+
+	$scope.isAuthenticated = function() {
+		return user.isLoggedIn();
+	};
+
+	$scope.logout = function() {
+		user.logout();
+		appService.$location.url("/login")
+	};
 }]);
 
 /*
